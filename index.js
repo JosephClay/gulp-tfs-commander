c4bf26a52431cc44510cc5143fefed72d0f80e5a
sasonic <xx1234xx@hotmail.com>
2016-09-27 19:14:52 -0700

    Works on all versions of visual studio
    recommitting so only AddVSLocationToPath shows as updated

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var _ = require('lodash'),
 	PLUGIN_NAME = 'gulp-tfs-commander',
 
 	defaults = {
-	    tfs: 'tf',
-	    command: 'edit',
-	    debug: false
+		tfs: 'tf',
+		command: 'edit',
+		debug: false
 	};
 var AddVSLocationToPath = function () {
     var platformPathSeporator = process.platform === 'win32' ? ';' : ':';
@@ -21,98 +21,98 @@ var AddVSLocationToPath = function () {
     pathToAdd += path.resolve(process.env[versionVSTools], '..\\IDE\\');
     process.env['PATH'] += pathToAdd;
 }
-var gulpTfs = module.exports = function (opts) {
-    opts = _.extend({}, defaults, opts);
+var gulpTfs = module.exports = function(opts) {
+	opts = _.extend({}, defaults, opts);
 
-    var hasUnlocked = false;
-    return through.obj(function (file, encoding, callback) {
-        var self = this;
+	var hasUnlocked = false;
+	return through.obj(function(file, encoding, callback) {
+		var self = this;
 
-        if (!fs.existsSync(file.path)) {
-            this.push(file);
-            return callback();
-        }
+		if (!fs.existsSync(file.path)) {
+			this.push(file);
+			return callback();
+		}
 
-        if (hasUnlocked) {
-            this.push(file);
-            return callback();
-        }
+		if (hasUnlocked) {
+			this.push(file);
+			return callback();
+		}
 
-        var command = utils.generateCommand(opts.tfs, opts.command + ' "' + file.path + '" ' + utils.zipParams(opts.params));
-        return exec(command, function (err, stdout, stderr) {
-            if (opts.debug) {
-                processExecResults(err, stdout, stderr);
-                utils.log('TFS result: command ' + opts.command + ' on file ' + gulpUtil.colors.cyan(stdout));
-            }
-            hasUnlocked = true;
-            self.push(file);
-            callback();
-        });
-    });
+		var command = utils.generateCommand(opts.tfs, opts.command + ' "' + file.path + '" ' + utils.zipParams(opts.params));
+		return exec(command, function(err, stdout, stderr) {
+			if (opts.debug) {
+				processExecResults(err, stdout, stderr);
+				utils.log('TFS result: command ' + opts.command + ' on file ' + gulpUtil.colors.cyan(stdout));
+			}
+			hasUnlocked = true;
+			self.push(file);
+			callback();
+		});
+	});
 };
 
-var direct = function (opts, file) {
-    opts = _.extend({}, defaults, opts);
+var direct = function(opts, file) {
+	opts = _.extend({}, defaults, opts);
 
-    var hasUnlocked = false;
-    return through.obj(function (f, encoding, callback) {
-        var self = this;
+	var hasUnlocked = false;
+	return through.obj(function(f, encoding, callback) {
+		var self = this;
 
-        if (!fs.existsSync(file)) {
-            this.push(f);
-            return callback();
-        }
+		if (!fs.existsSync(file)) {
+			this.push(f);
+			return callback();
+		}
 
-        if (hasUnlocked) {
-            this.push(f);
-            return callback();
-        }
+		if (hasUnlocked) {
+			this.push(f);
+			return callback();
+		}
 
-        var command = utils.generateCommand(opts.tfs, opts.command + ' "' + file + '" ' + utils.zipParams(opts.params));
-        return exec(command, function (err, stdout, stderr) {
-            if (opts.debug) {
-                processExecResults(err, stdout, stderr);
-                utils.log('TFS result: command ' + opts.command + ' on file ' + gulpUtil.colors.cyan(stdout));
-            }
-            hasUnlocked = true;
-            self.push(f);
-            callback();
-        });
-    });
+		var command = utils.generateCommand(opts.tfs, opts.command + ' "' + file + '" ' + utils.zipParams(opts.params));
+		return exec(command, function(err, stdout, stderr) {
+			if (opts.debug) {
+				processExecResults(err, stdout, stderr);
+				utils.log('TFS result: command ' + opts.command + ' on file ' + gulpUtil.colors.cyan(stdout));
+			}
+			hasUnlocked = true;
+			self.push(f);
+			callback();
+		});
+	});
 };
 
-var processExecResults = function (err, stdout, stderr) {
-    if (stderr) {
-        utils.log('TFS command error: ' + gulpUtil.colors.cyan(stderr) + ' -- ' + gulpUtil.colors.red(stderr));
-        return stderr;
-    }
+var processExecResults = function(err, stdout, stderr) {
+	if (stderr) {
+		utils.log('TFS command error: ' + gulpUtil.colors.cyan(stderr) + ' -- ' + gulpUtil.colors.red(stderr));
+		return stderr;
+	}
 
-    if (err) {
-        utils.log('TFS command caution: ' + gulpUtil.colors.cyan(stdout) + ' -- ' + gulpUtil.colors.yellow(err));
-        return err;
-    }
+	if (err) {
+		utils.log('TFS command caution: ' + gulpUtil.colors.cyan(stdout) + ' -- ' + gulpUtil.colors.yellow(err));
+		return err;
+	}
 
-    return stdout;
+	return stdout;
 };
 
-var checkForTFS = function (opts, done) {
-    var command = utils.generateCommand(opts.tfs, 'bob');
-    exec(command, function (err, stdout, stderr) {
-        // not a tf command, but validates that tf throws the right error
-        // this ensures that the tf command is available
-        if (stderr === 'Unrecognized command: bob.\r\n') {
-            return done();
-        }
+var checkForTFS = function(opts, done) {
+	var command = utils.generateCommand(opts.tfs, 'bob');
+	exec(command, function(err, stdout, stderr) {
+		// not a tf command, but validates that tf throws the right error
+		// this ensures that the tf command is available
+		if (stderr === 'Unrecognized command: bob.\r\n') {
+			return done();
+		}
 
-        throw new PluginError(PLUGIN_NAME, 'TF command is not available. Make sure Visual Studio is installed and its install directory is in your %PATH%');
-    });
+		throw new PluginError(PLUGIN_NAME, 'TF command is not available. Make sure Visual Studio is installed and its install directory is in your %PATH%');
+	});
 };
 AddVSLocationToPath();
 _.extend(gulpTfs, {
-    checkForTFS: checkForTFS,
-    direct: direct,
-    config: function (opts) {
-        _.extend(defaults, opts);
-        return gulpTfs;
-    }
+	checkForTFS: checkForTFS,
+	direct: direct,
+	config: function(opts) {
+		_.extend(defaults, opts);
+		return gulpTfs;
+	}
 });
